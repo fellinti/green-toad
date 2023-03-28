1b0eaa38caf110d79c693c1fb38c4f33a20ddbc1
Tim Fellin <tim.a.fellin@gmail.com>
2023-03-28 19:21:26 -0400

    yerp

--- .vscode/script.js
+++ .vscode/script.js
@@ -1,8 +1,16 @@
-// Select the photo element
-const photo = document.querySelector(".photo");
+// Replace the values below with your own image path, SoundCloud player URL, and tweet content
+const imagePath = 'your-image.jpg';
+const soundcloudUrl = 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/123456789&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true';
+const tweetContent = '<p lang="en" dir="ltr">Your tweet goes here.</p>&mdash; Your Twitter Name (@yourusername) <a href="https://twitter.com/yourusername/status/1234567890123456789?ref_src=twsrc%5Etfw">March 1, 2023</a>';
 
-// Add a click event listener to the photo
-photo.addEventListener("click", function () {
-  // Toggle the class to make the photo appear or disappear when clicked
-  photo.classList.toggle("hidden");
-});
+// Get references to the image, SoundCloud player, and tweet elements in the HTML file
+const imageElement = document.getElementById('dynamic-image');
+const soundcloudElement = document.getElementById('soundcloud-player');
+const tweetElement = document.getElementById('tweet');
+
+// Set the source of the image element to the imagePath variable
+imageElement.src = imagePath;
+
+// Set the HTML content of the SoundCloud player and tweet elements to the soundcloudUrl and tweetContent variables, respectively
+soundcloudElement.innerHTML = `<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="${soundcloudUrl}"></iframe>`;
+tweetElement.innerHTML = tweetContent;
